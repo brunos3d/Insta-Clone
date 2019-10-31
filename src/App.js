bee1f082baa8f5a48dc1c1e9d43c6e503a2a3119
Bruno Silva <bruno3dcontato@gmail.com>
2019-10-31 15:25:53 -0300

    navbar component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import Navbar from './components/Navbar';
 import Feed from './components/Feed';
 
 export default function App() {
 
     return (
         <Container>
+            <Navbar />
+
             <Feed />
         </Container>
     );
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 77px;
+    align-items: center;
+    background-color: #fff;
+    border-bottom: 1px solid #dbdbdb;
+    z-index: 1;
+`;
