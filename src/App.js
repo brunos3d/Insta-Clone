bbfaeced3af88b528fd75ae4a367df43e3420beb
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-03 04:32:45 -0300

    removed: shuffle function

--- src/App.js
+++ src/App.js
@@ -16,24 +16,6 @@ export default function App() {
         setCurrentPage(currentPage + 1);
     }
 
-    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    function shuffle(array) {
-        let currentIndex = array.length, temporaryValue, randomIndex;
-
-        while (0 !== currentIndex) {
-
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-
-        return array;
-    }
-
-
     useEffect(() => {
 
         async function fetchData() {
