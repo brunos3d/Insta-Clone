f126e23e9483a00bb97e4325aae7716c77717082
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-03 06:08:59 -0300

    CLI test: yarn build

---
+++ build/precache-manifest.61fe0603ed9baddef1b50684bd037aee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9be902be6ee9cfd023d3d0713852d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b7b973280d86943f250",
+    "url": "/static/js/2.35f91685.chunk.js"
+  },
+  {
+    "revision": "b0722e72a3272366934b",
+    "url": "/static/js/main.72956f23.chunk.js"
+  },
+  {
+    "revision": "39adfea6b6c6c6ef3db1",
+    "url": "/static/js/runtime-main.4afc8340.js"
+  }
+]);
